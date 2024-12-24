0af53eb081aca241313a6df377e5c08a00fa3d12
John Hartsfield <john@unilink.local>
2024-12-24 07:12:53 -0500

    updates

--- internal/components/stat/stat.js
+++ internal/components/stat/stat.js
@@ -1,22 +1,22 @@
 var urls = ["hrtsfld.xyz", "bolt-marketing.org", "mysterygift.org", "tagmachine.xyz", "walboard.xyz"];
 var i = urls.length - 1;
 var statuses = document.getElementsByClassName("status");
-console.log(i)
-async function everyTime() {
+
+function everyTime() {
     statuses[i].classList.add("OKcheck");
     statuses[i].innerHTML = "";
 
-    fetch("https://" + urls[i], {signal: AbortSignal.timeout(4000)})
+    fetch("https://" + urls[i], {signal: AbortSignal.timeout(14000)})
        .then((response) => {
-            if (response.ok) {
+            // if (response.ok) {
                 statuses[i].innerHTML = response.status;
                 statuses[i].classList.add("OK");
                 statuses[i].classList.remove("notOK");
-            } else {
-                statuses[i].innerHTML = "";
-                statuses[i].classList.add("notOK");
-                statuses[i].classList.remove("OK");
-            }
+            // } else {
+                // statuses[i].innerHTML = "";
+                // statuses[i].classList.add("notOK");
+                // statuses[i].classList.remove("OK");
+            // }
         })
         .catch((error_) => {
             statuses[i].innerHTML = "";
@@ -25,10 +25,10 @@ async function everyTime() {
             console.log(error_);
         })
         .finally(() => {
-            statuses[i].classList.remove("OKcheck");
             i = i - 1
             if (i == -1) {i = urls.length-1}
         });
+        statuses[i].classList.remove("OKcheck");
 }
 
 var myInterval = setInterval(everyTime, 1000);
