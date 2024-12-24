eeb8599d0c6ab3347bbf7be0cd52a3e734f3abd6
John Hartsfield <john@unilink.local>
2024-12-24 07:53:44 -0500

    updates

--- internal/components/stat/stat.js
+++ internal/components/stat/stat.js
@@ -3,32 +3,41 @@ var i = urls.length - 1;
 var statuses = document.getElementsByClassName("status");
 
 function everyTime() {
+    statuses[i].classList.remove("OK");
+    statuses[i].classList.remove("notOK");
     statuses[i].classList.add("OKcheck");
-    statuses[i].innerHTML = "";
-
-    fetch("https://" + urls[i], {signal: AbortSignal.timeout(14000)})
-       .then((response) => {
-            // if (response.ok) {
+    fetch("https://" + urls[i], {signal: AbortSignal.timeout(2500)})
+        .then((response) => {
+            console.log(urls[i], response)
+            if (response.ok) {
                 statuses[i].innerHTML = response.status;
-                statuses[i].classList.add("OK");
+                statuses[i].classList.remove("OKcheck");
                 statuses[i].classList.remove("notOK");
-            // } else {
-                // statuses[i].innerHTML = "";
-                // statuses[i].classList.add("notOK");
-                // statuses[i].classList.remove("OK");
-            // }
+                statuses[i].classList.add("OK");
+            } else {
+                statuses[i].innerHTML = "";
+                statuses[i].classList.remove("OKcheck");
+                statuses[i].classList.remove("OK");
+                statuses[i].classList.add("notOK");
+            }
         })
         .catch((error_) => {
             statuses[i].innerHTML = "";
-            statuses[i].classList.add("notOK");
             statuses[i].classList.remove("OK");
+            statuses[i].classList.remove("OKcheck");
+            statuses[i].classList.add("notOK");
             console.log(error_);
         })
         .finally(() => {
             i = i - 1
-            if (i == -1) {i = urls.length-1}
+            if (i == -1) {
+                i = urls.length-1
+            }
+            statuses[i].classList.remove("OK");
+            statuses[i].classList.remove("notOK");
+            statuses[i].classList.add("OKcheck");
+            statuses[i].innerHTML = "";
         });
-        statuses[i].classList.remove("OKcheck");
 }
-
-var myInterval = setInterval(everyTime, 1000);
+everyTime();
+var myInterval = setInterval(everyTime, 3000);
